e40331ab3fc51b20f18e3d50a646bbe238920e6f
Charles Cheng <cowton0517@gmail.com>
2022-11-24 10:11:11 +0800

    202211241011

--- GradientPrac/ViewController.swift
+++ GradientPrac/ViewController.swift
@@ -11,7 +11,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
+        
+        
+    }
+    
+    private func setupView() {
+        let size = UIScreen.main.bounds.size
+        var leftView = UIView(frame: CGRect(x: 0, y: 0,
+                                            width: size.width/2,
+                                            height: size.height))
+        leftView.backgroundColor = .yellow
+        view.addSubview(leftView)
+        
+        var leftPath = UIBezierPath(roundedRect: CGRect(x: 0, y: 0 + size.height/4,
+                                                        width: size.width/2,
+                                                        height: size.height/2),
+                                    byRoundingCorners: [.topRight, .bottomRight, .topLeft, .bottomLeft],
+                                    cornerRadii: CGSize(width: 150, height: 0))
+        
+        let leftShapeLayer = CAShapeLayer()
+        leftShapeLayer.path = leftPath.cgPath
+        leftView.layer.mask = leftShapeLayer
+        
+        
+        
     }
 
 
